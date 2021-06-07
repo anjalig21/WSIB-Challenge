da3408cf9e13f20d1fec6afc2641d0f787d33f53
Anjali Gupta <anjaligta@outlook.com>
2021-06-06 23:10:27 -0400

    Added Phase One to React Fundamentals

---
+++ wsib/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import PhaseOne from "./Components/Phase 1/phaseOne";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PhaseOne/>
+      </div>
+    );
+  }
+}
+
+export default App;
