a8955eda702a8efade5949f2323c81e0dc65c61e
Anjali Gupta <anjaligta@outlook.com>
2021-06-07 00:48:50 -0400

    updated styling

--- Problem One/src/App.js
+++ Problem One/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import PhaseOne from "./Components/Phase 1/phaseOne";
+import PhaseTwo from "./Components/Phase 2/phaseTwo";
+import PhaseThree from "./Components/Phase 3/phaseThree";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <PhaseOne/>
+        <PhaseTwo/>
+        <PhaseThree/>
       </div>
     );
   }
