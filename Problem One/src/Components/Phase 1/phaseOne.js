2a21118a60a35185e6a7314d9de65f51d5d3dab5
Anjali Gupta <anjaligta@outlook.com>
2021-06-07 00:49:30 -0400

    fixed bugs in phase 1

--- Problem One/src/Components/Phase 1/phaseOne.js
+++ Problem One/src/Components/Phase 1/phaseOne.js
@@ -3,32 +3,46 @@ import "./phaseOne.css";
 
 class PhaseOne extends Component {
   state = {
-    colorOne: 'blue',
-    colorTwo: 'purple'
-  }
+    colorOne: "blue",
+    colorTwo: "purple",
+  };
 
   onChangeButtonOne = () => {
-    if (this.state.colorOne === 'blue') {
-      this.setState({ colorOne: 'green' });
+    if (this.state.colorOne === "blue") {
+      this.setState({ colorOne: "green" });
     } else {
-      this.setState({ colorOne: 'blue' });
+      this.setState({ colorOne: "blue" });
     }
-  }
+  };
 
   onChangeButtonTwo = () => {
-    if (this.state.colorTwo === 'purple') {
-      this.setState({ colorTwo: 'red' });
+    if (this.state.colorTwo === "purple") {
+      this.setState({ colorTwo: "red" });
     } else {
-      this.setState({ colorTwo: 'purple' });
+      this.setState({ colorTwo: "purple" });
     }
-  }
+  };
 
   render() {
     return (
       <div className="phaseOne">
         <h1 className="phaseTitle"> Phase 1 </h1>
-        <button className="htmlButton" style={{ backgroundColor: this.state.colorOne }} onClick={this.onChangeButtonOne}>Click Me If You Like WSIB!</button>
-        <button className="htmlButton" style={{ backgroundColor: this.state.colorTwo }} onClick={this.onChangeButtonTwo}>Click Me If You Want To Get Hired!</button>
+        <button
+          className="htmlButton"
+          style={{ backgroundColor: this.state.colorOne }}
+          onClick={this.onChangeButtonOne}
+        >
+          Click Me If You Like WSIB!
+        </button>
+        <button
+          className="htmlButton"
+          style={{ backgroundColor: this.state.colorTwo }}
+          onClick={this.onChangeButtonTwo}
+        >
+          Click Me If You Want To Get Hired!
+        </button>
+        <br />
+        <br />
       </div>
     );
   }
